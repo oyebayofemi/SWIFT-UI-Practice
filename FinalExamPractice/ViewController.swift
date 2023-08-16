4fd589896139c3e70439f250981d25dcfe81a7ae
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-08-16 15:34:53 -0400

    Initial Commit

---
+++ FinalExamPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalExamPractice
+//
+//  Created by oyebayo oluwafemi on 2023-08-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
